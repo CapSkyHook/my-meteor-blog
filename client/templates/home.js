d0cf1a12bbbb30e3d03a8a66e17df2b8bd865679
Gautam Srinivasan <gautam.srinivasan7@gmail.com>
2015-03-09 13:35:35 -0700

    initial commit

---
+++ client/templates/home.js
@@ -0,0 +1,10 @@
+Template.home.created = function(){
+  console.log('Created the home template');
+};
+Template.home.rendered = function(){
+  console.log('Rendered the home template');
+};
+
+Template.home.destroyed = function(){
+  console.log('Destroyed the home template');
+};
